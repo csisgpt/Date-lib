99a548da09d13e93cbfc547e61897e71b787b93c
csisgpt <csisgpt@gmail.com>
2025-07-19 23:24:39 +0330

    feat: advanced duration interval and plugin system

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { DateTime } from './DateTime';
+export { DateTime, Plugin } from './DateTime';
 export { Duration } from './Duration';
 export { Interval } from './Interval';
 export * as utils from '../utils';
